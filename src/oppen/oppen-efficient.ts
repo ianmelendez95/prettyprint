dac1cf96041ff3e5549ccb9f6259f59e3780cb2e
Ian Melendez <imelendez@preludedynamics.com>
2022-05-07 11:18:26 -0500

    [oppen] initial 'efficient' commit

---
+++ src/oppen/oppen-efficient.ts
@@ -0,0 +1,18 @@
+/**
+ * Implementation of Oppens "Efficient but Less Simple Algorithm"
+ */
+
+import { Token } from "./oppen-tokens"
+
+class Scanner {
+  scan() {
+    let x
+    while ((x = this.receive())) {
+
+    }
+  }
+
+  receive(): Token {
+    return null
+  }
+}
