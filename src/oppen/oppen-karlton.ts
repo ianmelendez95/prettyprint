c66b491ed26ecb1350bc5e212263d93270c066f9
Ian Melendez <imelendez@preludedynamics.com>
2022-05-09 21:58:55 -0500

    [karlton] full (untested) transcription of Oppen-Karlton implementation

--- src/oppen/oppen-karlton.ts
+++ src/oppen/oppen-karlton.ts
@@ -35,6 +35,8 @@ const MAX_BLANKS = Number.MAX_SAFE_INTEGER
 const LINE_BREAK: Break = mkBreak(MAX_BLANKS)
 
 class PrettyPrinter {
+  outputBuffer: string[]
+
   margin: number
   space: number 
 
@@ -50,9 +52,9 @@ class PrettyPrinter {
   sizeInfinity: number = Number.MAX_SAFE_INTEGER
   scanStack: number[]
 
-  top: number 
-  bottom: number
-  printStack: PrintStack = new PrintStack(63)
+  // top: number 
+  // bottom: number
+  printStack: PrintStack = new PrintStack()
 
   // PrettyPrintInit
   constructor(lineWidth: number = 75) {
@@ -60,7 +62,6 @@ class PrettyPrinter {
 
     this.space = this.margin = lineWidth
     n = 3 * this.margin
-    this.top = this.bottom = 0
   }
 
   prettyPrint(t: Token) {
@@ -117,27 +118,177 @@ class PrettyPrinter {
     }
   }
 
-  prettyPrintInit() {}
+  checkStream() {
+    if (this.rightTotal - this.leftTotal > this.space) {
+      if (this.scanStack.length > 0) {
+        if (this.left = this.scanBottom()) {
+          this.size[this.scanPopBottom()] = this.sizeInfinity
+        }
+      }
 
-  checkStack(x: number) {}
+      this.advanceLeft(this.token[this.left], this.size[this.left])
+      if (this.left !== this.right) {
+        this.checkStream()
+      }
+    }
+  }
 
-  checkStream() {}
+  scanPush(x: number) {
+    this.scanStack.push(x)
+  }
 
-  advanceLeft(t: Token, l: number) {}
+  scanPop() {
+    const res = this.scanStack.pop()
+    if (typeof res === 'undefined') {
+      throw new Error('Cannot pop, scan stack is empty')
+    }
+    return res
+  }
 
-  advanceRight() {}
+  scanBottom(): number {
+    if (this.scanStack.length === 0) {
+      throw new Error('Cannot show bottom, scan stack is empty')
+    } else {
+      return this.scanStack[0]
+    }
+  }
 
-  indent(x: number) {}
+  scanTop(): number {
+    if (this.scanStack.length === 0) {
+      throw new Error('Cannot show top, scan stack is empty')
+    } else {
+      return this.scanStack.at(-1)
+    }
+  }
 
-  scanPush(x: number) {}
+  scanPopBottom(): number {
+    const bottom = this.scanStack.shift()
+    if (typeof bottom === 'undefined') {
+      throw new Error('Cannot pop bottom, scan stack is empty')
+    }
+    return bottom
+  }
 
-  print(t: Token, l: number) {}
+  advanceRight() {
+    this.right++
+  }
+
+  advanceLeft(x: Token, l: number) {
+    if (l < 0) {
+      return
+    }
+
+    this.print(x, l)
+
+    if (x.kind === 'break') {
+      this.leftTotal += x.blankSpace
+    } else if (x.kind === 'string') {
+      this.leftTotal += l
+    }
+
+    if (this.left !== this.right) {
+      this.left++
+      this.advanceLeft(this.token[this.left], this.size[this.left])
+    }
+  }
+
+  checkStack(k: number) {
+    if (this.scanStack.length === 0) {
+      return
+    }
+
+    const x: number = this.scanTop()
+    const t: Token = this.token[x]
+    if (t.kind === 'begin') {
+      if (k > 0) {
+        this.size[this.scanPop()] += this.rightTotal
+        this.checkStack(k - 1)
+      }
+    } else if (t.kind === 'end') {
+      this.size[this.scanPop()] = 1
+      this.checkStack(k + 1)
+    } else {
+      this.size[this.scanPop()] += this.rightTotal
+      if (k > 0) {
+        this.checkStack(k)
+      }
+    }
+  }
+
+  printNewLine(amount: number) {
+    this.outputBuffer.push('\n' + ' '.repeat(amount))
+  }
+
+  indent(amount: number) {
+    this.outputBuffer.push(' '.repeat(amount))
+  }
+
+  print(x: Token, l: number) {
+    if (x.kind === 'begin') {
+      if (l > this.space) {
+        this.printStack.push({
+          offset: this.space - x.offset,
+          break: x.breakType === 'consistent' ? 'consistent' : 'inconsistent'
+        })
+      } else {
+        this.printStack.push({ offset: 0, break: 'fits' })
+      }
+    } else if (x.kind === 'end') {
+      this.printStack.pop()
+    } else if (x.kind === 'break') {
+      const printTop = this.printStack.top()
+      if (printTop.break === 'fits') {
+          this.space -= x.blankSpace
+      } else if (printTop.break === 'consistent') {
+          this.space = this.printStack.top().offset - x.offset
+          this.printNewLine(this.margin - this.space)
+      } else if (printTop.break === 'inconsistent') {
+        if (l > this.space) {
+          this.space = printTop.offset - x.offset
+          this.printNewLine(this.margin - this.space)
+        } else {
+          this.space -= x.blankSpace
+        }
+      } 
+    } else if (x.kind === 'string') {
+      if (l > this.space) {
+        throw new Error('LineTooLong')
+      }
+
+      this.space -= l
+      this.outputBuffer.push(x.string)
+    } else {
+      throw new Error('Unhandled token type: ' + x.kind)
+    }
+  }
+}
+
+type PrintStackBreak = 'fits' | 'inconsistent' | 'consistent'
+
+type PrintStackEntry = {
+  offset: number,
+  break: PrintStackBreak
 }
 
 class PrintStack {
-  x: number
-  
-  constructor(x: number) {
-    x = x
+  index: number
+  items: PrintStackEntry[]
+
+  constructor(index = 0) {
+    this.index = index 
+    this.items = []
+  }
+
+  push(entry: PrintStackEntry) {
+    this.items.push(entry)
+  }
+
+  pop(): PrintStackEntry {
+    return this.items.pop()
+  }
+
+  top(): PrintStackEntry {
+    return this.items.at(-1)
   }
 }
+
